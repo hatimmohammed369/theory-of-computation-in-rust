75b0abf82786536d9714263528d68677c1c4db95
Hatim Muhammed <hatimmohammed369@gmail.com>
2023-07-27 11:05:30 +0200

    Constructor with Initialization

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,65 @@ impl NFA {
 	}
     }
 
+    pub fn init_new(states: &[&str], alphabet: &[&str], transition_function: &[(&str, &str, &[&str])], start_state: &str, accept_states: &[&str], is_deterministic: bool) -> NFA {
+	let mut states_set = states.iter().map(|x| String::from(*x)).collect::<HashSet<String>>();
+	let mut alphabet_set = alphabet.iter().map(|x| String::from(*x)).collect::<HashSet<String>>();
+
+	if !states.contains(&start_state) {
+	    eprintln!("Warning: Start State `{start_state}` is not in the states array");
+	}
+	states_set.insert(String::from(start_state));
+
+	accept_states.iter().for_each(
+	    |x| {
+		if !states.contains(x) {
+		    eprintln!("Warning: Accept State `{x}` is not in the states array");
+		}
+		states_set.insert(String::from(*x));
+	    }
+	);
+
+	let mut function = HashMap::<String, HashMap<String, HashSet<String>>>::new();
+	for item in transition_function {
+	    let (state, symbol, items) = (item.0, item.1, item.2);
+	    if !states.contains(&state) {
+		eprintln!("Warning: State `{state}` is not in the states array");
+		eprintln!("Found in transition {:?}", item);
+	    }
+
+	    if !alphabet.contains(&symbol) {
+		eprintln!("Warning: Symbol `{symbol}` is not in the alphabet array");
+		eprintln!("Found in transition {:?}", item);
+	    }
+	    alphabet_set.insert(String::from(symbol));
+
+	    let destination =
+		function
+		.entry(String::from(state)).or_insert(HashMap::new())
+		.entry(String::from(symbol)).or_insert(HashSet::new());
+
+	    items.iter().for_each(
+		|x| {
+		    destination.insert(String::from(*x));
+		    if !states.contains(x) {
+			eprintln!("Warning: State `{x}` is not in the states array");
+			eprintln!("Found in transition {:?}", item);
+		    }
+		}
+	    );
+	}
+
+	NFA {
+	    states: states_set,
+	    alphabet: alphabet_set,
+	    transition_function: function,
+	    start_state: String::from(start_state),
+	    accept_states: accept_states.iter().map(|x| String::from(*x)).collect(),
+	    is_deterministic,
+	    dfa: None
+	}
+    }
+
     pub fn write_state_symbols_map(&mut self, state: &str) -> &mut HashMap<String, HashSet<String>> {
 	    self.transition_function
 		.entry(state.to_string())
