594d97d3c129eee561574340ab8d0cc5504727d2
Hatim Muhammed <hatimmohammed369@gmail.com>
2023-07-27 11:00:43 +0200

    Additional methods to access transition function
    Have to sepearate methods two for reading and two for writing

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,16 @@ use std::collections::HashMap;
 pub struct NFA {
     pub states: HashSet<String>,
     pub alphabet: HashSet<String>,
-    pub transition_function:
-    HashMap<String, HashMap<String, HashSet<String>>>,
+    pub transition_function: HashMap<String, HashMap<String, HashSet<String>>>,
     pub start_state: String,
     pub accept_states: HashSet<String>,
     pub is_deterministic: bool,
     pub dfa: Option<Box<NFA>>
+}
+
+#[derive(Debug)]
+pub enum ComputationResult {
+    Accept, Reject
 }
 
 impl NFA {
@@ -26,27 +30,34 @@ impl NFA {
 	}
     }
 
-    pub fn state_symbols_map(&mut self, state: &str) ->
-	&mut HashMap<String, HashSet<String>>
-    {
+    pub fn write_state_symbols_map(&mut self, state: &str) -> &mut HashMap<String, HashSet<String>> {
 	    self.transition_function
 		.entry(state.to_string())
 		.or_insert(HashMap::new())
     }
 
-    pub fn symbol_states_set(&mut self, state: &str, symbol: &str) -> &mut HashSet<String> {
-	if !self.alphabet.contains(symbol) {
-	    eprintln!("Warning: Symbol {symbol} is not in alphabet {:?}", self.alphabet);
-	}
-	self.state_symbols_map(state)
+    pub fn write_symbol_states_set(&mut self, state: &str, symbol: &str) -> &mut HashSet<String> {
+	self.write_state_symbols_map(state)
 	    .entry(symbol.to_string())
 	    .or_insert(HashSet::new())
     }
 
+    pub fn read_state_symbols_map(&self, state: &str) -> Option<&HashMap<String, HashSet<String>>> {
+	self.transition_function.get(state)
+    }
+
+    pub fn read_symbol_states_set(&self, state: &str, symbol: &str) -> Option<&HashSet<String>> {
+	match self.read_state_symbols_map(state) {
+	    Some(state_symbols_map) => state_symbols_map.get(symbol),
+	    None => None
+	}
+    }
+
     pub fn add_transition<'a>(&'a mut self, state: &str, symbol: &str, output: impl Iterator<Item = &'a String>) {
+	let destination = self.write_symbol_states_set(state, symbol);
 	output.for_each(
 	    |x| {
-		self.symbol_states_set(state, symbol).insert(x.to_string());
+		destination.insert(x.to_string());
 	    }
 	);
     }
