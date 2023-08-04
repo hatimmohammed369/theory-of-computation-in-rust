41bfa2313503a163bbb43dbab73839fb1b4f335c
Hatim Muhammed <hatimmohammed369@gmail.com>
2023-08-04 20:46:20 +0200

    The Scanner Iterator

--- src/generators/regular_expressions.rs
+++ src/generators/regular_expressions.rs
@@ -231,14 +231,133 @@ pub fn concat_string_regexes(exprs: &[Option<&str>]) -> Option<String> {
     Some(concat_expr)
 }
 
+use std::collections::HashSet;
+
+#[derive(Debug)]
 enum TokenType {
     EmptyString, Symbol,
     RightParen, LeftParen,
-    Star, Pipe, End
+    Star, Pipe
 }
 
+#[derive(Debug)]
 struct Token {
     pub name: TokenType,
-    pub lexeme: String,
+    pub lexeme: char,
     pub position: usize
+}
+
+struct Scanner {
+    chars: Vec<char>,
+    pub alphabet: HashSet<char>,
+    empty_string_found: bool,
+    current: usize,
+}
+
+impl Scanner {
+    fn new(source: &str) -> Scanner {
+	Scanner {
+	    chars: source.chars().collect::<_>(),
+	    alphabet: HashSet::new(),
+	    empty_string_found: false,
+	    current: 0
+	}
+    }
+}
+
+impl Iterator for Scanner {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Token> {
+	let mut next = None;
+	
+	let peek = {
+	    if self.current >= self.chars.len() {
+		'\0'
+	    } else {
+		self.chars[self.current]
+	    }
+	};
+
+	let prev = {
+	    if self.current == 0 {
+		'\0'
+	    } else {
+		self.chars[self.current-1]
+	    }
+	};
+
+	if !self.empty_string_found {
+	    if (prev == '\0' && (peek == '\0' || peek == '|')) ||
+		(prev == '(' && (peek == '|' || peek == ')')) ||
+		(prev=='|' && (peek=='|' || peek==')' || peek=='\0')) {
+		    self.alphabet.insert('\0');
+		    next = Some(
+			Token {
+			    name: TokenType::EmptyString,
+			    lexeme: '\0',
+			    position: self.current
+			}
+		    );
+		}
+	    self.empty_string_found = true;
+	}
+
+	if next.is_none() && self.current < self.chars.len() {
+	    self.empty_string_found = false;
+	    
+	    match peek {
+		'(' => {
+		    next = Some(
+			Token {
+			    name: TokenType::LeftParen,
+			    lexeme: '(',
+			    position: self.current
+			}
+		    );
+		}
+		')' => {
+		    next = Some(
+			Token {
+			    name: TokenType::RightParen,
+			    lexeme: ')',
+			    position: self.current
+			}
+		    );
+		}
+		'*' => {
+		    next = Some(
+			Token {
+			    name: TokenType::Star,
+			    lexeme: '*',
+			    position: self.current
+			}
+		    );
+		}
+		'|' => {
+		    next = Some(
+			Token {
+			    name: TokenType::Pipe,
+			    lexeme: '|',
+			    position: self.current
+			}
+		    );
+		}
+		c => {
+		    self.alphabet.insert(c);
+		    next = Some(
+			Token {
+			    name: TokenType::Symbol,
+			    lexeme: c,
+			    position: self.current
+			}
+		    );
+		}
+	    }
+
+	    self.current += 1;
+	}
+	
+	next
+    }
 }
